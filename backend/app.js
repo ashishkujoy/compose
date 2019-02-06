0f6191380f2134e79034e3c32fd437a2c3a16edc
ashish kumar <ashishkujoy@gmail.com>
2019-02-06 17:13:20 +0530

    Initial commit

---
+++ backend/app.js
@@ -0,0 +1,60 @@
+const app = require('express')();
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT;
+let connectionString = process.env.DATABASE_URL || "postgres://localhost:5432/postgres";
+
+const { Client } = require('pg');
+const client = new Client(connectionString);
+const SECRET_MESSAGE = process.env.SECRET_MESSAGE;
+console.log(connectionString);
+
+app.use(bodyParser());
+
+app.get('/number', (req, res) => {
+    client.query('SELECT numbers from numbers', null, (err, result) => {
+        if(err) {
+            res.status(500);
+            res.send(err.message);
+            return;
+        }
+        res.json({ "numbers": result.rows });    
+    });
+    
+});
+
+app.post('/number', (req, res) => {
+    let number = req.body.number;
+    client.query(`insert into numbers values('${number}')`,null,(err,number) => {
+        if(err) {
+            res.status(500);
+            res.end();
+        } else {
+            res.status(201);
+            res.end();
+        }
+        
+    });
+});
+
+app.get('/', (req, res) => {
+    console.log('Recieve a request');
+    res.send(`<h1>${SECRET_MESSAGE}</h1>`);
+});
+
+client.connect()
+.then(function(){
+    client.query('CREATE TABLE IF NOT EXISTS numbers(numbers integer);',(err,result) => {
+        if(err) {
+            console.log(`Error occur while creating table ${err.message}`);
+        } else {
+            app.listen(PORT, () => {
+                console.log(`App listening on ${PORT}`);
+            });
+        }
+    });
+        
+})
+.catch(function(err){
+    console.log(err);
+})
+
---
+++ loadBalancer/balancer.js
@@ -0,0 +1,18 @@
+const httpProxy = require('http-proxy');
+const app = require('express')();
+const hosts = process.env.HOSTS.split(',');
+let counter = 1;
+const PORT = process.env.PORT || 9000;
+
+app.get('/', (req, res) => {
+    const proxyServer = httpProxy.createProxyServer({ target: hosts[counter % hosts.length] });
+    counter++;
+    proxyServer.proxyRequest(req, res, {});
+});
+
+app.listen(PORT, (err) => {
+    if (err) console.log(err);
+    else console.log(`Listening on ${PORT}`);
+});
+
+app.on('error', (err) => console.log(err.message));
