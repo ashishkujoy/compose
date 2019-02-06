d5f103abe2448d67a64976e190df2a2a97b35672
ashishm <ashishmahi46@gmail.com>
2019-02-06 20:47:34 +0530

    Bind mount the loadBalancer code for hot reloading start using nodemon for running loadbalancer app. update loadbalancer server to proxy every request

--- loadBalancer/balancer.js
+++ loadBalancer/balancer.js
@@ -1,18 +1,19 @@
-const httpProxy = require('http-proxy');
-const app = require('express')();
-const hosts = process.env.HOSTS.split(',');
-let counter = 1;
-const PORT = process.env.PORT || 9000;
+    const httpProxy = require('http-proxy');
+    const app = require('express')();
+    const hosts = process.env.HOSTS.split(',');
+    let counter = 1;
+    const PORT = process.env.PORT || 9000;
 
-app.get('/', (req, res) => {
-    const proxyServer = httpProxy.createProxyServer({ target: hosts[counter % hosts.length] });
-    counter++;
-    proxyServer.proxyRequest(req, res, {});
-});
 
-app.listen(PORT, (err) => {
-    if (err) console.log(err);
-    else console.log(`Listening on ${PORT}`);
-});
+    app.use((req,res)=> {
+        const proxyServer = httpProxy.createProxyServer({ target: hosts[counter % hosts.length] });
+        counter++;
+        proxyServer.proxyRequest(req, res, {});
+    })
 
-app.on('error', (err) => console.log(err.message));
+    app.listen(PORT, (err) => {
+        if (err) console.log(err);
+        else console.log(`Listening on ${PORT}`);
+    });
+
+    app.on('error', (err) => console.log(err.message));
