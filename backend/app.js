0560a950581ea0d3303ff7360e8674cbec646fd2
ashish kumar <ashishkujoy@gmail.com>
2019-02-07 00:22:53 +0530

    Add health and sleep endpoints

--- backend/app.js
+++ backend/app.js
@@ -2,38 +2,47 @@ const app = require('express')();
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT;
 let connectionString = process.env.DATABASE_URL || "postgres://localhost:5432/postgres";
-
 const { Client } = require('pg');
 const client = new Client(connectionString);
 const SECRET_MESSAGE = process.env.SECRET_MESSAGE;
-console.log(connectionString);
+const delayTime = process.env.DELAY_TIME;
 
 app.use(bodyParser());
 
 app.get('/number', (req, res) => {
     client.query('SELECT numbers from numbers', null, (err, result) => {
-        if(err) {
+        if (err) {
             res.status(500);
             res.send(err.message);
             return;
         }
-        res.json({ "numbers": result.rows });    
+        res.json({ "numbers": result.rows });
     });
-    
+
 });
 
 app.post('/number', (req, res) => {
     let number = req.body.number;
-    client.query(`insert into numbers values('${number}')`,null,(err,number) => {
-        if(err) {
+    client.query(`insert into numbers values('${number}')`, null, (err, number) => {
+        if (err) {
             res.status(500);
             res.end();
         } else {
             res.status(201);
             res.end();
         }
-        
+
     });
+});
+
+app.get('/health', (req, res) => {
+    res.end();
+});
+
+app.get('/sleep', (req, res) => {
+    setTimeout(() => {
+        res.json({ message: "apnaa time aayega" });
+    }, delayTime);
 });
 
 app.get('/', (req, res) => {
@@ -42,19 +51,19 @@ app.get('/', (req, res) => {
 });
 
 client.connect()
-.then(function(){
-    client.query('CREATE TABLE IF NOT EXISTS numbers(numbers integer);',(err,result) => {
-        if(err) {
-            console.log(`Error occur while creating table ${err.message}`);
-        } else {
-            app.listen(PORT, () => {
-                console.log(`App listening on ${PORT}`);
-            });
-        }
-    });
-        
-})
-.catch(function(err){
-    console.log(err);
-})
+    .then(function () {
+        client.query('CREATE TABLE IF NOT EXISTS numbers(numbers integer);', (err, result) => {
+            if (err) {
+                console.log(`Error occur while creating table ${err.message}`);
+            } else {
+                app.listen(PORT, () => {
+                    console.log(`App listening on ${PORT}`);
+                });
+            }
+        });
+
+    })
+    .catch(function (err) {
+        console.log(err);
+    })
 
--- loadBalancer/balancer.js
+++ loadBalancer/balancer.js
@@ -1,19 +1,20 @@
-    const httpProxy = require('http-proxy');
-    const app = require('express')();
-    const hosts = process.env.HOSTS.split(',');
-    let counter = 1;
-    const PORT = process.env.PORT || 9000;
+const httpProxy = require('http-proxy');
+const app = require('express')();
+const hosts = process.env.HOSTS.split(',');
+let counter = 1;
+const PORT = process.env.PORT || 9000;
 
 
-    app.use((req,res)=> {
-        const proxyServer = httpProxy.createProxyServer({ target: hosts[counter % hosts.length] });
-        counter++;
-        proxyServer.proxyRequest(req, res, {});
-    })
+app.use((req, res) => {
+    const proxyServer = httpProxy.createProxyServer({ target: hosts[counter % hosts.length] });
+    proxyServer.on('error', () => res.end());
+    counter++;
+    proxyServer.proxyRequest(req, res);
+})
 
-    app.listen(PORT, (err) => {
-        if (err) console.log(err);
-        else console.log(`Listening on ${PORT}`);
-    });
+app.listen(PORT, (err) => {
+    if (err) console.log(err);
+    else console.log(`Listening on ${PORT}`);
+});
 
-    app.on('error', (err) => console.log(err.message));
+app.on('error', (err) => console.log(err.message));
